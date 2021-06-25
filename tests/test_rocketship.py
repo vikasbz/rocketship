f9d7b1cbde605d94c34f96c6838166b604a7437f
Vikas Z <Vikas Z vikas.zingade@skillate.com>
2021-06-25 12:07:42 +0530

    tdd: test Django version

--- tests/test_rocketship.py
+++ tests/test_rocketship.py
@@ -1,5 +1,16 @@
+import django
+
 from rocketship import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_django():
+    print(f"Django version: {django.__version__}")
+
+
+if __name__ == "__main__":
+    test_version()
+    test_django()
