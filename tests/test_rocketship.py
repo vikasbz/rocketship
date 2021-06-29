b3228e6c54ab252c7cf2a5433314060166405fa0
Vikas Z <Vikas Z vikas.zingade@skillate.com>
2021-06-29 09:45:40 +0530

    add simple test case for test project

--- tests/test_rocketship.py
+++ tests/test_rocketship.py
@@ -1,16 +1,8 @@
-import django
+from django.test import SimpleTestCase
 
 from rocketship import __version__
 
 
-def test_version():
-    assert __version__ == "0.1.0"
-
-
-def test_django():
-    print(f"Django version: {django.__version__}")
-
-
-if __name__ == "__main__":
-    test_version()
-    test_django()
+class TestProject(SimpleTestCase):
+    def test_version(self):
+        assert __version__ == "0.1.0"
