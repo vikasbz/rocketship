05094a07365138e881c048efb9a5200ad0f8a867
Vikas Z <Vikas Z vikas.zingade@skillate.com>
2021-08-12 12:13:42 +0530

    enlist urls from profiles

--- rocketship/urls.py
+++ rocketship/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    include("profiles/", include("profiles.urls")),
+    path("profiles/", include("profiles.urls")),
 ]
